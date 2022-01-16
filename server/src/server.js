41638365fad8532bed3586d5bfe930345275bf03
rigole <foplacide@gmail.com>
2022-01-16 23:11:16 +0100

    final configuration of json before starting 2

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const  app = require('./app')
+app.listen()
+
+const server = http.createServer(app)
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
+const PORT = process.env.PORT || 8000;
+console.log(PORT)
+
