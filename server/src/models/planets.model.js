bfabe7ff4742ec7eb97879e315d69a2473fb3b5e
rigole <foplacide@gmail.com>
2022-01-18 09:26:47 +0100

    setting the planets model to return planet to the controller

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,37 @@
-const planets = []
+import fs from "fs";
+const parse = require('csv-parse')
+//const fs = require('fs');
+const isHabitablePlanets = [];
 
-module.exports = planets
+
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6
+}
+
+fs.createReadStream('kepler_data.csv')
+
+    .pipe(parse({
+        comment: "#",
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)){
+            isHabitablePlanets.push(data)
+        }
+
+    })
+    .on('error', (error)=>{
+        console.log(error)
+    })
+    .on('end', () => {
+        console.log(isHabitablePlanets.map((planet) =>{
+            return planet['kepler_name']
+        }))
+        // console.log(isHabitablePlanets)
+    })
+// parse()
+
+module.exports = {
+    planets: isHabitablePlanets,
+}
