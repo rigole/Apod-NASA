9cc6dfdffd5d0cc2224a66308dcfbe34d12e7ec3
rigole <foplacide@gmail.com>
2022-02-10 09:43:51 +0100

    setting useLaunches and requests 1

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -31,6 +31,13 @@ async function httpGetLaunches() {
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
+    await fetch(`${API_URL}/launches`, {
+        method: "post",
+        headers: {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(launch),
+    })
 }
 
 async function httpAbortLaunch(id) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -12,7 +12,21 @@ function httpGetAllLaunches(req, res){
 function httpAddNewLaunch(req, res) {
     const launch = req.body
 
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination){
+        return res.status(400).json({
+            error: "Missing required launch Information"
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate)
+    if(isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: "Invalid launch date",
+        })
+    }
+
+
+
     addNewLaunch(launch)
     res.status(201)
     return res.status(201).json(launch)
