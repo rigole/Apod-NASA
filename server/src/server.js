307f0b51109d6e5191b0a820eddc23f70d3b9e4d
rigole <foplacide@gmail.com>
2022-03-01 11:15:10 +0100

    installing mongoose for using MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,15 @@ const  app = require('./app')
 
 
 const { loadPlanetsData } = require('./models/planets.model')
+
+const MONGO_URL = "mongodb+srv://plass:123@nasa-cluster.exsxa.mongodb.net/nasaDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app)
+
 async function startServer(){
     await loadPlanetsData();
+
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
     })
